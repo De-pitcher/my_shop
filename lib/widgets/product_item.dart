f53138c77d752a7dbb9774366cc06fb388398a37
De-pitcher <emmanwa000@gmail.com>
2022-12-29 20:01:45 +0100

    ADD: snack bar to the product overview screen to confirm the addition of product to the cart and a button to remove before two seconds elapses

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -42,6 +42,19 @@ class ProductItem extends StatelessWidget {
             icon: const Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.title, product.price);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text('Added item to cart!'),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).colorScheme.secondary,
           ),
