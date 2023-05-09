09e3f0c89164ccf5380754295dfb9b20c238c62d
De-pitcher <emmanwa000@gmail.com>
2023-05-09 14:46:35 +0100

    Added the auth card to the widgets folder

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -5,9 +5,14 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Loading...'),
+        child: Column(
+          children: const [
+            CircularProgressIndicator(),
+            Text('Loading...'),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/auth_card.dart
@@ -0,0 +1,237 @@
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/auth.dart';
+import '../utils/app_color.dart';
+import '../utils/utils.dart';
+import '../utils/email_validator.dart';
+
+enum AuthMode { signup, login }
+
+class AuthCard extends StatefulWidget {
+  const AuthCard({super.key});
+
+  @override
+  State<AuthCard> createState() => _AuthCardState();
+}
+
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  AuthMode _authMode = AuthMode.login;
+  final Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+  var _isLoading = false;
+  final _passwordController = TextEditingController();
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 360),
+    );
+    _opacityAnimation = Tween<double>(
+      begin: 0.0,
+      end: 10.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.bounceIn,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _showdDialog(String errorMessage) {
+    customDialog(
+      title: 'An Error Occurred!',
+      msg: errorMessage,
+      context: context,
+      isConfirmedHandler: () => Navigator.of(context).pop(),
+    );
+  }
+
+  void _toggleIsLoadingTo(bool value) {
+    setState(() {
+      _isLoading = value;
+    });
+  }
+
+  void _submit() async {
+    if (!_formKey.currentState!.validate()) {
+      // Invalid!
+      return;
+    }
+    _formKey.currentState!.save();
+    _toggleIsLoadingTo(true);
+    if (_authMode == AuthMode.login) {
+      // Log user in
+      await Provider.of<Auth>(context, listen: false)
+          .login(
+            _authData['email']!,
+            _authData['password']!,
+          )
+          .then((value) => _toggleIsLoadingTo(false))
+          .onError((error, stackTrace) => {
+                _toggleIsLoadingTo(false),
+                _showdDialog(error.toString()),
+              });
+    } else {
+      // Sign user up
+      await Provider.of<Auth>(context, listen: false)
+          .signUp(
+            _authData['email']!,
+            _authData['password']!,
+          )
+          .then((value) => _toggleIsLoadingTo(false))
+          .onError((error, stackTrace) => {
+                _toggleIsLoadingTo(false),
+                _showdDialog(error.toString()),
+              });
+    }
+  }
+
+  void _switchAuthMode() {
+    if (_authMode == AuthMode.login) {
+      setState(() {
+        _authMode = AuthMode.signup;
+      });
+      _controller.forward();
+    } else {
+      setState(() {
+        _authMode = AuthMode.login;
+      });
+      _controller.reverse();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+    return Card(
+      color: AppColor.bg,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 8.0,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 360),
+        curve: Curves.slowMiddle,
+        height: _authMode == AuthMode.signup ? 320 : 260,
+        constraints: BoxConstraints(
+          minHeight: _authMode == AuthMode.signup ? 320 : 260,
+        ),
+        width: deviceSize.width * 0.75,
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'E-Mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) =>
+                      value!.isValidEmail() ? null : 'Invalid email!',
+                  onSaved: (value) {
+                    _authData['email'] = value!;
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  controller: _passwordController,
+                  validator: (value) =>
+                      value != null && (value.isEmpty || value.length < 5)
+                          ? 'Password is too short!'
+                          : null,
+                  onSaved: (value) {
+                    _authData['password'] = value!;
+                  },
+                ),
+                AnimatedContainer(
+                  constraints: BoxConstraints(
+                    minHeight: _authMode == AuthMode.signup ? 60 : 0,
+                    maxHeight: _authMode == AuthMode.signup ? 120 : 0,
+                  ),
+                  duration: const Duration(milliseconds: 360),
+                  curve: Curves.easeIn,
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: TextFormField(
+                      enabled: _authMode == AuthMode.signup,
+                      decoration:
+                          const InputDecoration(labelText: 'Confirm Password'),
+                      obscureText: true,
+                      validator: _authMode == AuthMode.signup
+                          ? (value) {
+                              if (value != _passwordController.text) {
+                                return 'Passwords do not match!';
+                              }
+                              return null;
+                            }
+                          : null,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                if (_isLoading)
+                  const CircularProgressIndicator()
+                else
+                  ElevatedButton(
+                    onPressed: _submit,
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      backgroundColor: Theme.of(context).primaryColor,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 8.0),
+                      textStyle: TextStyle(
+                        color: Theme.of(context)
+                            .primaryTextTheme
+                            .labelLarge!
+                            .color,
+                      ),
+                    ),
+                    child: Text(
+                      _authMode == AuthMode.login ? 'LOGIN' : 'SIGN UP',
+                    ),
+                  ),
+                TextButton(
+                  onPressed: _switchAuthMode,
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 4,
+                    ),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    textStyle: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  child: Text(
+                    '${_authMode == AuthMode.login ? 'SIGNUP' : 'LOGIN'} INSTEAD',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
